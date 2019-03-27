5f7ec82143ffb21ae549aa1a1b0a670c646f2b01
DiegoDarkHeart <evertonsantosmelo@hotmail.com>
2019-03-28 03:38:52 +0900

    calculo e resolução do programa

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -1,5 +1,5 @@
 $(document).on('click','#btnFinalisar',function(){
-  var var_name = $("input[name='optradio']:checked").val();
+  var atividade = $("input[name='optradio']:checked").val();
 
   var idade = parseFloat($("#idade").val());
   var altura = parseFloat($("#altura").val());
@@ -7,4 +7,17 @@ $(document).on('click','#btnFinalisar',function(){
 
   var sexo = $("#sexo option:selected").val();
   
+  switch(atividade){
+    case 1: atividade = parseFloat(1.2); break;
+    case 2: atividade = parseFloat(1.375); break;
+    case 3: atividade = parseFloat(1.55); break;
+    case 4: atividade = parseFloat(1.725); break;
+    case 5: atividade = parseFloat(1.9); break;
+  }
+  if(sexo == h){
+  /*Cálculo para Homens:*/var MB = atividade * {66 + [(13.7 * peso) + ( 5 * altura) - (6.8 * idade)]}
+  }
+  else{ 
+  /*Cálculo para Mulheres:*/ MB = atividade * {655 + [(9.6 * peso) + (1.8 * altura) - (4.7 * idade)]}
+  }
 });
